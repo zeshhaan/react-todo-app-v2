1057eb24631804d2ee31b8953b2276f352954529
Mohammed Zeeshan <34814964+zeshhaan@users.noreply.github.com>
2020-12-09 18:06:26 +0530

    Added todo edit functionality

--- src/index.js
+++ src/index.js
@@ -22,6 +22,24 @@ function TodoApp() {
     setTodosList(todosList.filter((i) => i !== item));
   }
 
+  const [edit, setEdit] = useState(null);
+  console.log(edit);
+
+  function handleUpdate(e) {
+    e.preventDefault();
+    const updatedTodos = todosList.map((todo) => {
+      console.log(todo);
+      if (todo.id === edit.id) {
+        return { ...todo, text: e.target[0].value };
+      } else {
+        return todo;
+      }
+    });
+
+    setTodosList(updatedTodos);
+    setEdit(null);
+  }
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -29,12 +47,24 @@ function TodoApp() {
         <button type="submit">ADD</button>
       </form>
       <ul>
-        {todosList.map((item) => (
-          <div key={item.id}>
-            <li>{item.text}</li>
-            <button onClick={() => removeItem(item)}>DEL</button>
-          </div>
-        ))}
+        {todosList.map((item) => {
+          if (edit?.id === item.id) {
+            return (
+              <form onSubmit={handleUpdate}>
+                <input defaultValue={edit.text} />
+                <button>SAV</button>
+              </form>
+            );
+          } else {
+            return (
+              <div key={item.id}>
+                <li>{item.text}</li>
+                <button onClick={() => removeItem(item)}>DEL</button>
+                <button onClick={() => setEdit(item)}>EDIT</button>
+              </div>
+            );
+          }
+        })}
       </ul>
     </div>
   );
