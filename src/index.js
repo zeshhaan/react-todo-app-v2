64b7c6d01d2dd5d5a42df8e55c5113bc84ecde3d
Mohammed Zeeshan <34814964+zeshhaan@users.noreply.github.com>
2020-12-16 15:15:48 +0530

    Added local storage functionality Todos now saves into local storage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import "./styles.css";
 import ReactDOM from "react-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function TodoApp() {
   const [todo, setTodo] = useState("");
   //initialise the todos List as array
-  const [todosList, setTodosList] = useState([]);
+  const [todosList, setTodosList] = useState(
+    () => JSON.parse(window.localStorage.getItem("todosList")) || []
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem("todosList", JSON.stringify(todosList));
+  }, [todosList]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -19,6 +25,34 @@ function TodoApp() {
   }
 
   function removeItem(item) {
+    // todosList = [{id: 1, name: 'a'}, {id: 2, name: 'b'}]
+    // this method -> .filter() -> returns an array
+    // (todo) => todo !== item)
+
+    // goal => delete name = 'a'
+    // todosList.filter(
+    //todo => todo.name !== 'a' -> true
+
+    // );
+    // // an array = [
+    //   {id: 2, name: 'b'}
+
+    // ]
+
+    // for loop -> iterate each array (
+    //   todo = {id: 1, name: 'a'}
+    // )
+    // setTodosList(todosList.filter((i) => i !== item));
+
+    // setTodosList(todosList.filter(
+    //   function(i) {
+    //     var i;
+    //     for (i = 0; i < cars.length; i++) {
+    //       text += cars[i] + "<br>";
+    //     }
+    //   }
+    // )
+
     setTodosList(todosList.filter((i) => i !== item));
   }
 
@@ -43,14 +77,14 @@ function TodoApp() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input value={todo} type="text" onChange={handleChange} />
+        <input value={todo} type="search" onChange={handleChange} />
         <button type="submit">ADD</button>
       </form>
       <ul>
         {todosList.map((item) => {
           if (edit?.id === item.id) {
             return (
-              <form onSubmit={handleUpdate}>
+              <form key={item.id} onSubmit={handleUpdate}>
                 <input defaultValue={edit.text} />
                 <button>SAV</button>
               </form>
