15c5b95d8b462c26a58d7879f2063d160253a5c5
Mohammed Zeeshan <34814964+zeshhaan@users.noreply.github.com>
2021-01-03 10:42:33 +0530

    Added stylings and autoFocus

--- src/index.js
+++ src/index.js
@@ -75,26 +75,75 @@ function TodoApp() {
   }
 
   return (
-    <div>
+    <div className="container">
       <form onSubmit={handleSubmit}>
-        <input value={todo} type="search" onChange={handleChange} />
-        <button type="submit">ADD</button>
+        <input
+          autoFocus
+          value={todo}
+          onChange={handleChange}
+          placeholder="New todo..."
+        />
+        <button type="submit"></button>
       </form>
       <ul>
         {todosList.map((item) => {
           if (edit?.id === item.id) {
             return (
               <form key={item.id} onSubmit={handleUpdate}>
-                <input defaultValue={edit.text} />
-                <button>SAV</button>
+                <input
+                  autoFocus
+                  defaultValue={edit.text}
+                  onFocus={(e) => e.currentTarget.select()}
+                />
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
               </form>
             );
           } else {
             return (
               <div key={item.id}>
                 <li>{item.text}</li>
-                <button onClick={() => removeItem(item)}>DEL</button>
-                <button onClick={() => setEdit(item)}>EDIT</button>
+                <div className="button-wrapper">
+                  <button onClick={() => removeItem(item)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+
+                  <button onClick={() => setEdit(item)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             );
           }
