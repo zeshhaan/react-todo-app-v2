cf4d5c9d9648bb1ca7701b11465046268c1cb905
Mohammed Zeeshan <34814964+zeshhaan@users.noreply.github.com>
2021-01-06 00:05:12 +0530

    Added mark as complete functionality + stylings Added mark as complete functionality with the help of Andrew
    Resources: https://github.com/andrewli-ca/todo/blob/7562e64d072fcdd4fdd425d4960a17e171a17fe0/src/app.js
    
    https://codesandbox.io/s/react-todo-app-forked-v64b9?file=/src/index.js
    
    Also added strikethrough styles

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function TodoApp() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    setTodosList([...todosList, { id: uuidv4(), text: todo }]);
+    setTodosList([...todosList, { id: uuidv4(), text: todo, complete: false }]);
     setTodo("");
   }
 
@@ -24,45 +24,28 @@ function TodoApp() {
     setTodo(e.target.value);
   }
 
+  function toggleComplete(item) {
+    const updatedTodos = todosList.map((todo) => {
+      if (todo.id === item.id) {
+        return { ...todo, complete: !todo.complete };
+      } else {
+        return todo;
+      }
+    });
+    setTodosList(updatedTodos);
+  }
+
   function removeItem(item) {
-    // todosList = [{id: 1, name: 'a'}, {id: 2, name: 'b'}]
-    // this method -> .filter() -> returns an array
-    // (todo) => todo !== item)
-
-    // goal => delete name = 'a'
-    // todosList.filter(
-    //todo => todo.name !== 'a' -> true
-
-    // );
-    // // an array = [
-    //   {id: 2, name: 'b'}
-
-    // ]
-
-    // for loop -> iterate each array (
-    //   todo = {id: 1, name: 'a'}
-    // )
-    // setTodosList(todosList.filter((i) => i !== item));
-
-    // setTodosList(todosList.filter(
-    //   function(i) {
-    //     var i;
-    //     for (i = 0; i < cars.length; i++) {
-    //       text += cars[i] + "<br>";
-    //     }
-    //   }
-    // )
-
     setTodosList(todosList.filter((i) => i !== item));
   }
 
   const [edit, setEdit] = useState(null);
-  console.log(edit);
+  //console.log(edit);
 
   function handleUpdate(e) {
     e.preventDefault();
     const updatedTodos = todosList.map((todo) => {
-      console.log(todo);
+      //console.log(todo);
       if (todo.id === edit.id) {
         return { ...todo, text: e.target[0].value };
       } else {
@@ -110,25 +93,18 @@ function TodoApp() {
                 </button>
               </form>
             );
-          } else {
+          } else if (item.complete === true) {
             return (
-              <div key={item.id}>
-                <li>{item.text}</li>
+              <li key={item.id} className="strikethrough">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={item.complete}
+                    onChange={() => toggleComplete(item)}
+                  />
+                  <span>{item.text}</span>
+                </label>
                 <div className="button-wrapper">
-                  <button onClick={() => removeItem(item)}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </button>
-
                   <button onClick={() => setEdit(item)}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -143,8 +119,63 @@ function TodoApp() {
                       />
                     </svg>
                   </button>
+                  <button onClick={() => removeItem(item)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
                 </div>
-              </div>
+              </li>
+            );
+          } else {
+            return (
+              <li key={item.id}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={item.complete}
+                    onChange={() => toggleComplete(item)}
+                  />
+                  <span>{item.text}</span>
+                </label>
+                <div className="button-wrapper">
+                  <button onClick={() => setEdit(item)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                      <path
+                        fillRule="evenodd"
+                        d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  <button onClick={() => removeItem(item)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </li>
             );
           }
         })}
